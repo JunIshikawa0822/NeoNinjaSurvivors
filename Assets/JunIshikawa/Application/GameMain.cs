6e41177eeb9b84254bf816a4dc148fbd46849890
JunIshikawa0822 <jun.ffcc83@gmail.com>
2024-05-16 18:30:31 +0900

    Revert "Merge remote-tracking branch 'origin/feature_masatoshiwatanabe' into integrate_1"
    This reverts commit ba8c41e8f18781a7cf81af7fa8f5e08ce58848a6, reversing
    changes made to cb5030b7b474059c6f12ed5d1b974bfa8d60fe7e.

---
+++ Assets/JunIshikawa/Application/GameMain.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour
+{
+    [SerializeField]
+    GameStatus gameStat;
+
+    List<SystemBase> allSystems;
+
+    List<IOnUpdate> allUpdateSystems;
+    
+
+    private void Awake()
+    {
+        allSystems = new List<SystemBase>()
+        {
+            new MoveSystem(),
+            new EnemySystem()
+        };
+
+        allUpdateSystems = new List<IOnUpdate>();
+
+        foreach (SystemBase system in allSystems)
+        {
+            //GameStatの値をSystemたちに全て読み込ませる　これによりSystemたちはGameStatusの値全てを自由に使うことができる
+            system.GameStatusInit(gameStat);
+
+            //SystemたちをallSystemに一旦ぶち込んだ後で、それぞれのInterfaceに従って分類する
+            //if (system is IOnFixedUpdate) allFixedUpdateSystems.Add(system as IOnFixedUpdate);
+            //if (system is IOnPreUpdate) allPreUpdateSystems.Add(system as IOnPreUpdate);
+            if (system is IOnUpdate) allUpdateSystems.Add(system as IOnUpdate);
+            //if (system is IOnLateUpdate) allLateUpdateSystems.Add(system as IOnLateUpdate);
+        }
+
+        //Debug.Log(string.Join(",", allUpdateSystems));
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (IOnUpdate system in allUpdateSystems) system.OnUpdate();
+    }
+}
---
+++ Assets/JunIshikawa/System/EnemySystem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySystem : SystemBase, IOnUpdate
+{
+    public void OnUpdate()
+    {
+        
+    }
+}
