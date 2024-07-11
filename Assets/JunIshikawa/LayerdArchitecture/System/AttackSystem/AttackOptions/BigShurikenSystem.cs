e948b8ba5fd1d26fd0b48be2f5e80c7554302250
odagakishita <122857213+odagakishita@users.noreply.github.com>
2024-07-11 22:11:30 +0900

    BigShurikenちょっとだけ

---
+++ Assets/JunIshikawa/LayerdArchitecture/System/AttackSystem/AttackOptions/BigShurikenSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigShurikenSystem : AttackOptionBase, IOnUpdate
+{
+    public void OnUpdate(){
+        // if (gameStat.bulletList.Count > 0)
+        // {
+        //     for (int i = gameStat.bulletList.Count - 1; i >= 0; i--)
+        //     {
+        //         gameStat.bulletList[i].OnUpdate();
+        //     }
+        // }
+    }
+}
